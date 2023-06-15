c637e7608543859e3759e53fa97e53dc58229d33
Ledeuadian <adrianjrfabian@gmail.com>
2023-06-15 22:18:58 +0800

    prompt route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,11 +5,10 @@ export const GET = async (request) => {
     try {
         await connectToDB();
 
-        const allPrompts = await Prompt.find({}).populate('creator');
-        console.log(allPrompts);
-        return new Response(JSON.stringify(allPrompts),{
-            status:200})
+        const allPrompts = await Prompt.find({}).populate('creator')
+        
+        return new Response(JSON.stringify(allPrompts), { status:200 })
     } catch (error) {
-        return new Response("Failed to fetch all prompts", {status:500})
+        return new Response("Failed to fetch all prompts", { status:500 })
     }
 }
