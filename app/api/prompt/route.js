99552aa5fbeeb46b4db8c7befbe10607ab71d004
Ledeuadian <adrianjrfabian@gmail.com>
2023-06-29 23:19:42 +0800

    all prompt fix f

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,15 +1,27 @@
-import Prompt from '@models/prompt';
-import { connectToDB } from '@utils/database';
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
 export const GET = async (request) => {
-    try {
-        await connectToDB();
+  try {
+    await connectToDB();
 
-        const allPrompts = await Prompt.find({}).populate('creator');
-       
-        return new Response(JSON.stringify(allPrompts), { status:200 })
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", { status:500 })
-    }
- 
-}
+    const prompts = await Prompt.find({}).sort({ _id: -1 }).populate("creator");
+
+    const response = new Response(JSON.stringify(prompts), { status: 200 });
+
+    // Add a unique identifier to the URL to force a cache-busting reload
+    const url = new URL(request.url);
+    url.searchParams.set("t", Date.now());
+    response.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Location", url.toString());
+
+    return response;
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,27 +1,17 @@
-import { connectToDB } from "@utils/database";
-import Prompt from "@models/prompt";
+import Prompt from '@models/prompt';
+import { connectToDB } from '@utils/database';
 
-export const GET = async (request) => {
-  try {
-    await connectToDB();
+export const GET = async (request, {params}) => {
+    try {
+        await connectToDB();
 
-    const prompts = await Prompt.find({}).sort({ _id: -1 }).populate("creator");
+        const prompts = await Prompt.find({
+            creator: params.id
+        }).populate('creator');
 
-    const response = new Response(JSON.stringify(prompts), { status: 200 });
-
-    // Add a unique identifier to the URL to force a cache-busting reload
-    const url = new URL(request.url);
-    url.searchParams.set("t", Date.now());
-    response.headers.set(
-      "Cache-Control",
-      "no-cache, no-store, must-revalidate"
-    );
-    response.headers.set("Pragma", "no-cache");
-    response.headers.set("Expires", "0");
-    response.headers.set("Location", url.toString());
-
-    return response;
-  } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
-  }
-};
+        return new Response(JSON.stringify(prompts),{
+            status:200})
+    } catch (error) {
+        return new Response("Failed to fetch prompts", {status:500})
+    }
+}   
